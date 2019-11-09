d47e03b52c6458f53c36ae38543505d83f8993b3
HaedHutner <mvvasilev@outlook.com>
2019-11-09 22:07:12 +0200

    Add some code to the placeholder

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,12 +6,25 @@
 #include <FastNoise/FastNoise.h>
 #include <stb/stb_image.h>
 
-int main() {
+const GLfloat TRIANGLE_VERTICES[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f,  1.0f, 0.0f,
+};
+
+const GLuint TRIANGLE_INDICES[] = {
+    0, 1, 2,
+    2, 1, 3
+};
+
+int main()
+{
     GLFWwindow *window;
 
-    glfwSetErrorCallback([](int error, const char * desc){ fputs(desc, stderr); });
+    glfwSetErrorCallback([](int error, const char *desc) { fputs(desc, stderr); });
 
-    if (!glfwInit()) {
+    if (!glfwInit())
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -23,32 +36,62 @@ int main() {
 
     window = glfwCreateWindow(800, 600, "placeholder", nullptr, nullptr);
 
-    if (!window) {
+    if (!window)
+    {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         std::cout << "Failed to initialize OpenGL context" << std::endl;
         return -1;
     }
 
     printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
 
-    glClearColor(135.0/255.0, 206.0/255.0, 235.0/255.0, 1.0);
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-    while (!glfwWindowShouldClose(window)) {
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(TRIANGLE_VERTICES), TRIANGLE_VERTICES, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(TRIANGLE_INDICES), TRIANGLE_INDICES, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    glClearColor(135.0 / 255.0, 206.0 / 255.0, 235.0 / 255.0, 1.0);
+
+    while (!glfwWindowShouldClose(window))
+    {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glColor3b(0.1, 0.1, 0.1);
-        glVertex2d(0, 0);
-        glVertex2d(0, 1);
-        glVertex2d(1, 0);
-        glEnd();
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(vao);
+
+        glVertexAttribPointer(
+            0,       
+            3,       
+            GL_FLOAT,
+            GL_FALSE,
+            0,       
+            (void *) 0
+        );
+
+        glDrawElements(GL_TRIANGLES, sizeof(TRIANGLE_INDICES), GL_UNSIGNED_INT, NULL);
+
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
