c4dfa6efee6405c3a9d7f9d2f90fbbe1e65a68d9
HaedHutner <mvvasilev@outlook.com>
2019-11-09 14:12:17 +0200

    Fix CMake

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,9 @@
+#include <glad/glad.h>
 #include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+#include <FastNoise/FastNoise.h>
+#include <stb/stb_image.h>
 
 int main() {
-    
+    GLFWwindow* window = glfwCreateWindow(4, 4, "test", nullptr, nullptr);
 }
