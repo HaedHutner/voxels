0de5a1388282b4a2a3154489ad599768aea57fd9
HaedHutner <mvvasilev@outlook.com>
2019-11-09 14:46:58 +0200

    Placeholder project works

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <glm/glm.hpp>
@@ -5,5 +7,47 @@
 #include <stb/stb_image.h>
 
 int main() {
-    GLFWwindow* window = glfwCreateWindow(4, 4, "test", nullptr, nullptr);
+    GLFWwindow *window;
+
+    glfwSetErrorCallback([](int error, const char * desc){ fputs(desc, stderr); });
+
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(800, 600, "placeholder", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
 }
